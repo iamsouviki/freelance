b70f83fa584216b730faed7b62b3efd7109363ae
Souvik <souvikghosh687@gmail.com>
2022-07-04 23:24:36 +0530

    raise pr

--- pages/welcome-page/index.jsx
+++ pages/welcome-page/index.jsx
@@ -3,7 +3,7 @@ import { Box, Flex } from "@chakra-ui/react";
 export default function welcomePage(){
     return(
     <Box>
-        <Flex>Hi</Flex>
+        <Flex>Hi Hi</Flex>
     </Box>
     );
 }
